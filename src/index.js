1e8962cca5c9fb91e10b172e3aa1c6277893540c
Irina-coud <irinatobolich313@gmail.com>
2021-03-15 22:41:13 +0300

    fix: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = true;
+  let consequens = str;
+
+  while (consequens.length > 0 && result === true) {
+
+    let notMatchCount = bracketsConfig.reduce((acc, brackets) => {
+      let concatConfig = brackets[0] + brackets[1];
+
+      if (consequens.indexOf(concatConfig) !== -1) {
+        consequens = consequens.replace(concatConfig, "");
+      } else {
+        acc++;
+      }
+      return acc;
+    }, 0);
+
+    result = bracketsConfig.length !== notMatchCount;
+  }
+
+
+  return result;
 }
+
